247c70d2201740ef6a0b855c5476b11f99bd88c7
Trinidad Gil <mtgil@uc.cl>
2022-11-09 23:29:27 -0300

    chucha

--- index.php
+++ index.php
@@ -17,15 +17,27 @@
   <br>
   <br>
 
-  <h3 align="center"> ¿Quieres buscar un Pokemón por su ID?</h3>
+  <h3 align="center"> ¿Quieres buscar numero de entradas de cortesia de un artista?</h3>
 
-  <form align="center" action="consultas/consulta_stats.php" method="post">
-    Id:
-    <input type="text" name="id_elegido">
-    <br/><br/>
+  <?php require("config/conexion.php");
+  $result = $db -> prepare("SELECT DISTINCT nombre_artistico FROM artistas ORDER BY nombre_artistico ASC;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
+
+  <form align="center" action="consultas2.php" method="post">
+    Seleccione a un Artista:
+    <select name="nombre">
+      <?php
+      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+      foreach ($dataCollected as $d) {
+        echo '<option value="'.$d[0].'">'.$d[0].'</option>';
+      }
+      ?>
+    </select>
+    <br><br>
     <input type="submit" value="Buscar">
   </form>
-  
   <br>
   <br>
   <br>
