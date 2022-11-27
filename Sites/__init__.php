6b646fdecc19093375a03f3ede52dfcb74607af0
tereporzio <mtporzio@uc.cl>
2022-11-27 17:14:51 -0300

    sites

---
+++ Sites/__init__.php
@@ -0,0 +1,52 @@
+
+<!--CODIGO PROYECTO EJEMPLO -->
+
+
+<?php
+
+// Este archivo se puede importar en cada archivo .php, y se puede tener
+// aquí todo lo necesario para no tener que agregarlo para cada archivo.
+// No tiene nada de mágico, es un archivo php cualquiera con un nombre arbitrario.
+
+// Cargamos los datos para poder iniciar la BDD
+require_once __DIR__ . "/config/data.php";
+
+// Se conecta a la BDD
+require_once __DIR__ . "/config/conexion.php";
+
+// Se crea siempre una sesión
+session_start();
+
+
+// Funciones propias de utilidad
+
+/**
+ * Volver al inicio, tiene que se llamada antes de entregar HTML.
+ */
+function go_home() {
+  header("Location: " . '/~grupo157/');
+  exit();
+}
+?>
+
+
+<!--CODIGO PROYECTO BELEN -->
+
+require_once __DIR__ . "/config/data.php";
+
+// Se conecta a la BDD
+require_once __DIR__ . "/config/conexion.php";
+
+// Se crea siempre una sesión
+session_start();
+
+
+// Funciones propias de utilidad
+
+/**
+ * Volver al inicio, tiene que se llamada antes de entregar HTML.
+ */
+function go_home() {
+  header('Refresh: 0; url = ./index.php');
+  exit();
+}
---
+++ Sites/login.php
@@ -0,0 +1,111 @@
+
+<!--CODIGO PROYECTO NUESTRO-->
+
+<?php
+require_once './__init__.php';
+
+
+// Vemos si se esta mandando el form o se está recibiendo
+$request_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+if ($request_method  === 'POST') {
+    // Se está recibiendo datos para el login
+?>
+
+
+
+
+<!--CODIGO PROYECTO BELEN-->
+
+<?php
+require_once './__init__.php';
+
+// Vemos si se esta mandando el form o se está recibiendo
+$request_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+
+#Cuando hay algn adentro:
+if ($request_method  === 'POST') {
+  // Se está recibiendo datos para el login
+  // Aquí se tendría que buscar el nombre del usuario y la contraseña en la BDD 
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  #session_start(); ???  
+
+  // Se guardan estos valores en la sesión
+  //$_SESSION['username'] = $username;
+  //$_SESSION['password'] = $password;
+  if ($username == ''){
+    go_home();
+  }
+
+  $usuarios = $DB1->query('SELECT * FROM usuarios;');
+  $usuarios = $usuarios-> fetchAll();
+
+  foreach ($usuarios as $u) {
+    if($u[1] == $username and $u[2] == $password){
+      $_SESSION['user_id'] = $u[0];
+      $_SESSION['username'] = $u[1];
+      $_SESSION['tipo'] = $u[3];
+      // go_home();
+    }
+  }
+
+  // Mandamos al usuario al inicio
+  go_home();
+} elseif ($request_method === 'GET') { 
+  // En este caso, que se trata de obtener la página de inicio de sesión
+  // y no hay una sesión iniciada, se muestra el form
+
+  include './templates/header.html'; ?>
+  <!-- https://bulma.io/documentation/columns -->
+  
+<?php include './templates/footer.html';
+} ?>
+
+
+<!--CODIGO PROYECTO EJEMPLO -->
+
+<?php
+require_once './__init__.php';
+
+// Vemos si se esta mandando el form o se está recibiendo
+$request_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+if ($request_method  === 'POST') {
+  // Se está recibiendo datos para el login
+
+  // Aquí se tendría que buscar el id del usuario en la BDD con el mail y la contraseña
+  $user_id = 1;
+  $user_name = "Ash";
+
+  // Se guardan estos valores en la sesión
+  $_SESSION['user_id'] = $user_id;
+  $_SESSION['user_name'] = $user_name;
+
+  // Mandamos al usuario al inicio
+  go_home();
+} elseif ($request_method === 'GET') {
+  // En este caso, que se trata de obtener la página de inicio de sesión
+  // y no hay una sesión iniciada, se muestra el form
+
+  include './templates/header.php'; ?>
+  <!-- https://bulma.io/documentation/columns -->
+  <section class="section">
+
+    <div class="columns is-mobile is-centered is-vcentered cover-all">
+      <div class="column is-half">
+        <!-- https://bulma.io/documentation/form/general/ -->
+        <form method="POST">
+          <div class="field">
+            <label class="label">Nombre de usuario</label>
+            <div class="control">
+              <input class="input" type="text" name="username" value="Ash">
+            </div>
+          </div>
+          <button class="button is-primary" type="submit" name="login">Login</button>
+        </form>
+      </div>
+    </div>
+  </section>
+<?php include './templates/footer.php';
+} ?>
