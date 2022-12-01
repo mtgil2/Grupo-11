4bd8b925921469861368a0790237f5f6c29c34f4
Trinidad Gil <mtgil@uc.cl>
2022-12-01 16:22:01 -0300

    data

--- config/data.php
+++ config/data.php
@@ -1,8 +1,10 @@
 <?php
-  $user = 'grupo11';
-  $password = 'tigre11';
-  $databaseName = 'grupo11e3';
 
-  $user2 = 'grupo12';
-  $password2 = 'grupo12';
-  $databaseName2 = 'grupo12e3';
+
+$user = 'grupo11';
+$password = 'tigre11';
+$databaseName = 'grupo11e3';
+
+$user2 = 'grupo12';
+$password2 = 'grupo12';
+$databaseName2 = 'grupo12e3';
