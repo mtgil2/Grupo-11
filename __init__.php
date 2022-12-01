a4bd34be876fab3d6537aa9dd0adbaf801416ff2
Trinidad Gil <mtgil@uc.cl>
2022-12-01 19:05:42 -0300

    init

--- __init__.php
+++ __init__.php
@@ -9,13 +9,13 @@
 // No tiene nada de mágico, es un archivo php cualquiera con un nombre arbitrario.
 
 // Cargamos los datos para poder iniciar la BDD
-require_once __DIR__ . "/config/data.php";
+#require_once __DIR__ . "/config/data.php";
 
 // Se conecta a la BDD
-require_once __DIR__ . "/config/conexion.php";
+#require_once __DIR__ . "/config/conexion.php";
 
 // Se crea siempre una sesión
-session_start();
+#session_start();
 
 
 // Funciones propias de utilidad
@@ -23,15 +23,18 @@ session_start();
 /**
  * Volver al inicio, tiene que se llamada antes de entregar HTML.
  */
-function go_home() {
-  header("Location: " . '/~grupo11/');
-  exit();
-}
-?>
+#function go_home() {
+ # header("Location: " . '/~grupo11/');
+ ## exit();
+#}
 
 
-<!--CODIGO PROYECTO BELEN -->
-<!--
+
+// Este archivo se puede importar en cada archivo .php, y se puede tener
+// aquí todo lo necesario para no tener que agregarlo para cada archivo.
+// No tiene nada de mágico, es un archivo php cualquiera con un nombre arbitrario.
+
+// Cargamos los datos para poder iniciar la BDD
 require_once __DIR__ . "/config/data.php";
 
 // Se conecta a la BDD
@@ -49,8 +52,12 @@ session_start();
 function go_home() {
   header('Refresh: 0; url = ./index.php');
   exit();
-
-
 }
 
--->
+/**
+ * Ejemplo de componentes, donde uno llama a la función y se genera HTML
+ * NOTA: Aunque esto esta funcional, el nombre de las columnas es el mismo
+ *       que en la BDD, se podria modificar para poder elegir que nombre mostrar.
+ *       [[Ver PDOStatement::fetch](https://www.php.net/manual/es/pdostatement.fetch.php)]
+ *
+ * @param PDOStatement $query
