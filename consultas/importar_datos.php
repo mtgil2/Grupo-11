d8eca2a0d228a4ba252f822a17df8973a4b66dc7
Felipe <fguzma@uc.cl>
2022-12-02 13:04:39 -0300

    qer

--- consultas/importar_datos.php
+++ consultas/importar_datos.php
@@ -27,7 +27,7 @@ require_once './../__init__.php';
 <br>
 <br>
 <form align="center" action="../index.php" method= 'post'>
-  <input class="button is-primary" type="submit" value="Volver" style = "height: 30px" >
+  <input class="button is-primary" type="submit" value="Volver" style = "height: 35px" >
 </form>
 
 <br>
