45d40483ca6975d7dac398b5674f69a340d7450c
Felipe <fguzma@uc.cl>
2022-12-02 13:26:30 -0300

    asdf

--- inicio_artistas.php
+++ inicio_artistas.php
@@ -1,1 +1,26 @@
-<button class="button"  type="submit" name="Importar">Importar datos</button>
+<?php
+require_once "./__init__.php";
+?>
+
+<?php include('./templates/header.html'); ?>
+
+
+<?php
+  require("./config/conexion.php");
+
+  $nombre_artista = $_SESSION['username'];
+
+  $query = "SELECT nombre_artista
+  FROM artistas
+  WHERE nombre_artista = '$nombre_artista'";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $data = $result -> fetchAll();
+
+  foreach ($data as $v) {
+    $nombre_artista = $v[0];
+}
+  ?>
+
+<h2 class="title is-1"> Bienvenido artista <?php echo $nombre_artista ?>
+</h2>
