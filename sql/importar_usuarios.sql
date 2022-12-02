fc4fbe2693a895be58b02e058e057eca384a7731
Trinidad Gil <mtgil@uc.cl>
2022-12-02 15:45:03 -0300

    otro

---
+++ sql/importar_usuarios.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION
+
+-- declaramos la función y sus argumentos
+importar_usuarios (id int,  username varchar(30), contrasena varchar(30), tipo varchar(30))
+
+-- declaramos lo que retorna, en este caso un booleano
+RETURNS INTEGER as $$
+
+
+
+-- definimos nuestra función
+BEGIN
+
+    -- verificar si existe la columna generation, si no existe la agregamos y seteamos todos los valores de esa columna en 1
+    --IF username NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='pokemon1') THEN
+    --    ALTER TABLE pokemon1 ADD generation int;
+    --    UPDATE pokemon1 SET generation = 1;
+    --END IF;
+
+    -- si el id en el argumento no está en la tabla, agregamos el pokemon
+    -- notar que ahora debemos agregar el dato de la columna generation en el values a insertar
+    -- INSERT INTO usuarios VALUES(user_name , password , tipo , id_tipo);
+    IF user_name NOT IN (SELECT usuarios.username from usuarios) THEN
+        INSERT INTO usuarios values(id,username, contrasena , tipo);
+
+        -- retornamos true si se agregó el valor
+        RETURN 1;
+    ELSE
+        -- y false si no se agregó
+        RETURN 0;
+
+    END IF;
+
+
+
+-- finalizamos la definición de la función y declaramos el lenguaje
+END
+$$ language plpgsql
