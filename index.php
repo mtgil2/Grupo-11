bd90aedd7d522520b66ba92242ebf2899bf118df
Felipe <fguzma@uc.cl>
2022-12-01 21:08:53 -0300

    qe

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ require_once "./__init__.php";
   <section class="section" >
     <div>
       <form class="buttons" action="consultas/importar_datos.php">
-      <button class="button" style= "background-color: #066173" type="submit" name="Importar">Importar datos</button>
+      <button class="button" style= "background-color: #2dadc6" type="submit" name="Importar">Importar datos</button>
       </form>
   </div>
   </section>
