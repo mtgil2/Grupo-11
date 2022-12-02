de8629520aeca56e29dc52219b84b455b8923847
Felipe <fguzma@uc.cl>
2022-12-02 12:28:40 -0300

    sdf

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ require_once "./__init__.php";
 
   <?php }} else { ?>
     <!-- En el caso que no, se muestran los botones para iniciar sesión -->
-    <img src="./styles/coldplay-1.jpg" alt="HTML5 Icon" style="width:400px;height:400px; margin-top: 0px" align = "right">
+    <img src="./styles/coldplay-1.jpg" alt="HTML5 Icon" style="width:400px;height:400px; margin-top: -100px" align = "right">
 <div style = "padding-left: 30px">
   <section class="section" >
     <div>
